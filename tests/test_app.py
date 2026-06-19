f8ef7ccd4d9264ed1b79afc2eb89e70f346ddec0
Gabriel Carvalho <gabrielcarvalhopsilva@outlook.com>
2026-06-19 11:02:22 -0300

    setup: introduces endpoints, tests and the project setup

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from src.app import app
+
+
+def test_root():
+    client = TestClient(app)                   # Arrange
+    response = client.get("/")                    # Act
+
+    assert response.status_code == HTTPStatus.OK           # Assert
+    assert response.json() == {"message": "Olá Mundo!"}    # Assert
