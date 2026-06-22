d277db28a5d5be48bb2cd4f64360d5d6b9d7834d
Gabriel Carvalho <gabrielcarvalhopsilva@outlook.com>
2026-06-21 22:38:00 -0300

    minor: renames packages

--- migrations/env.py
+++ migrations/env.py
@@ -1,12 +1,10 @@
 from logging.config import fileConfig
 
-from sqlalchemy import engine_from_config
-from sqlalchemy import pool
-
 from alembic import context
+from sqlalchemy import engine_from_config, pool
 
-from pythando.models import table_registry
-from pythando.settings import Settings
+from pypeople.models import table_registry
+from pypeople.settings import Settings
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
